9a47765ed0e586f31ec9fd2fb0648f5ea81a2455
Alisa Kataeva <alisakataeva999@gmail.com>
2024-03-26 21:31:26 +0500

    баг 6.8 - стоимость указана с приставкой "от"

--- src/components/Solutions/RentSolutions/RentSolutions.jsx
+++ src/components/Solutions/RentSolutions/RentSolutions.jsx
@@ -22,11 +22,11 @@ function rentSolutions({rentSolutions}) {
 
                     <div className={item.name == 'Pro' ? 'rentSolutions__hover-bg active' : 'rentSolutions__hover-bg'}>
                         <div className='rentSolutions__name'>{item.name}</div>
-                        <div className='rentSolutions__priceOld'>{item.priceOld}<span className='rentSolutions__span'> ₽ / год</span>
+                        <div className='rentSolutions__priceOld'>от {item.priceOld}<span className='rentSolutions__span'> ₽ / год</span>
                         </div>
-                        <div className='rentSolutions__price'>{item.price}<span className='rentSolutions__span'> ₽ / год</span>
+                        <div className='rentSolutions__price'>от {item.price}<span className='rentSolutions__span'> ₽ / год</span>
                         </div>
-                        <div className='rentSolutions__price-month'>{item.priceMonth}<span
+                        <div className='rentSolutions__price-month'>от {item.priceMonth}<span
                             className='rentSolutions__span'> ₽ / мес</span></div>
                     </div>
 
@@ -89,11 +89,11 @@ function rentSolutions({rentSolutions}) {
                             <div
                                 className={item.name == 'Pro' ? 'rentSolutions__hover-bg active' : 'rentSolutions__hover-bg'}>
                                 <div className='rentSolutions__name'>{item.name}</div>
-                                <div className='rentSolutions__priceOld'>{item.priceOld}<span
+                                <div className='rentSolutions__priceOld'>от {item.priceOld}<span
                                     className='rentSolutions__span'> ₽ / год</span></div>
-                                <div className='rentSolutions__price'>{item.price}<span className='rentSolutions__span'> ₽ / год</span>
+                                <div className='rentSolutions__price'>от {item.price}<span className='rentSolutions__span'> ₽ / год</span>
                                 </div>
-                                <div className='rentSolutions__price-month'>{item.priceMonth}<span
+                                <div className='rentSolutions__price-month'>от {item.priceMonth}<span
                                     className='rentSolutions__span'> ₽ / мес</span></div>
                             </div>
 
